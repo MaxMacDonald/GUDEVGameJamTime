b675ac55066d387ac5614658f2415ef0d7d9a4e3
MaxMacDonald <maximacd@gmail.com>
2026-03-10 11:44:32 +0000

    added a main menu and escape function, setup for basic enemy

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.escapeKey.isPressed)
+        {
+            ToMainMenu();
+        }
+    }
+
+    public void ToMainMenu()
+    {
+        //Check for escape button press
+        SceneManager.LoadScene(0);
+    }
+}
